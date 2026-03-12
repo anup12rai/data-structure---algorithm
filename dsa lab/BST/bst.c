9a1451be00bc377f5345bc569816820973a4312a
anup <aniruddharai39@gmail.com>
2026-03-12 09:10:34 +0545

    git commit
    git commit

---
+++ dsa lab/BST/bst.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+};
+struct Node* createNode(int key)
+{
+    struct Node *temp;
+      temp = (struct Node*)malloc(sizeof(struct Node));
+    if(temp == NULL){
+        printf("Memory allocation failed");
+        exit(1);
+    }
+
+    temp->key = key;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+int insertChild(struct Node *node, int key){
+    if(key < node->key){
+        if(node->left == NULL){
+            node->left = createNode(key);
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    else if(key > node->key){
+        if(node->right == NULL){
+            node->right = createNode(key);
+            return 2;
+        }
+        else{
+            return -2;
+        }
+    }
+
+    else{
+        return -3;
+    }
+}
+
+int main(){
+    struct Node *root;
+    int r;
+    root = createNode(10);
+    r = insertChild(root,5);
+    printf("%d\n",r);
+    r = insertChild(root,15);
+    printf("%d\n",r);
+    r = insertChild(root,5);
+    printf("%d\n",r);
+
+}
